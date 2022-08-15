4046da2e1f01d898be91944580f9700c0c25f508
Sakyuba Noe <398noe@gmail.com>
2022-08-15 17:16:40 +0900

    Challenged 09〜13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,13 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
+  var flag = false;
 
+  const checker = document.getElementById("check");
+  const text = document.getElementById("text");
+
+  if (checker.checked) {
+    text.style.backgroundColor = "red";
+  } else {
+    text.style.backgroundColor = "transparent";
+  }
 }
